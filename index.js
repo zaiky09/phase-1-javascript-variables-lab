3c63af4a20a0afb1f4bf35bd823002500ee6c192
zaiky09 <zamilmozamil@outlook.com>
2023-02-02 11:49:44 +0300

    Variables declared

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+//let companyName = "Scuber";
+
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
